b11640adc74ac90e98465a35f57038fae74a367d
aylakemura-sys <ayla.kemura@escola.pr.gov.br>
2026-06-29 16:56:51 -0300

    fsaf

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca. forEach(button => {
-button.addEventListener ('click', function() )
-{
-    const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+avanca.forEach(button => {
+    button.addEventListener('click', function() {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-    atual.classList.remove('ativo')
-    document.getElementByI(proximoPasso).classList.add('ativo');
- }
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
 
-})
+const botaoTema = document.getElementById('alternar-tema');
+
+botaoTema.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
