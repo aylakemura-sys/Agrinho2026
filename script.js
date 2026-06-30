a157760c3e7c2a87a23aa460d98b60a0f927095f
aylakemura-sys <ayla.kemura@escola.pr.gov.br>
2026-06-29 21:18:18 -0300

    sadwa

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ const botaoTema = document.getElementById('alternar-tema'); // Busca na tela o b
 botaoTema.addEventListener('click', function() { // Adiciona um "ouvinte" que espera o clique no botão de tema
     document.body.classList.toggle('dark-mode'); // Liga ou desliga a classe 'dark-mode' no corpo do site a cada clique
 });
+
+
+const btnSubmitName = document.getElementById('botao-enviar-nome');
+const inputName = document.getElementById('nome-usuario');
+const dynamicResponseDiv = document.getElementById('resposta-dinamica');
+const welcomeMessage = document.getElementById('mensagem-boas-vindas');
+
+// Função para processar o nome e exibir a saudação inicial
+btnSubmitName.addEventListener('click', () => {
+    // Armazena o nome digitado em uma variável tirando espaços extras
+    const userName = inputName.value.trim();
+
+    // Valida se o campo não está vazio
+    if (userName !== "") {
+        // Altera o texto da mensagem de forma funcional e personalizada
+        welcomeMessage.innerHTML = Olá <strong>${userName}</strong>,seja bem vindo!;
+        
+        // Remove a classe para mostrar a mensagem e o próximo botão
+        dynamicResponseDiv.classList.remove('elemento-oculto');
+    } else {
+        alert("Por favor, digite o seu nome antes de enviar.");
+    }
